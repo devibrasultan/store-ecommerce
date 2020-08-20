aa1c5dc3bc3b573b0fe6544ddf421a529197e999
devibrasultan <devibrasultan@gmail.com>
2020-08-20 14:27:50 +0300

    =end first branch

---
+++ routes/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "admin" middleware group. Now create something great!
+|
+*/
+
+// note that prefix is admin all file route
+
+Route::group(['namespace' => 'Dashboard', 'middleware' => 'auth:admin'],function (){
+    Route::get('/', 'DashboardController@index') -> name('admin.dashboard'); // the first page admin visits if authentication
+});
+
+Route::group(['namespace' => 'Dashboard', 'middleware' => 'guest:admin'],function (){
+    Route::get('/login','LoginController@login') -> name('admin.login');
+    Route::post('/login','LoginController@postLogin') -> name('admin.post.login');
+});
