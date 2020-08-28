8db5bc9c5c04423d26fc6c301223052a17eeb428
devibrasultan <devibrasultan@gmail.com>
2020-08-28 15:05:57 +0300

    =begin branch

--- routes/admin.php
+++ routes/admin.php
@@ -15,11 +15,23 @@ use Illuminate\Support\Facades\Route;
 
 // note that prefix is admin all file route
 
-Route::group(['namespace' => 'Dashboard', 'middleware' => 'auth:admin'],function (){
-    Route::get('/', 'DashboardController@index') -> name('admin.dashboard'); // the first page admin visits if authentication
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ //...
+
+        Route::group(['namespace' => 'Dashboard', 'middleware' => 'auth:admin', 'prefix' => 'admin'],function (){
+            Route::get('/', 'DashboardController@index') -> name('admin.dashboard'); // the first page admin visits if authentication
+            Route::group(['prefix' => 'settings'],function(){
+            Route::get('shipping-methods/{type}','SettingsController@editShippingMethods')->name('edit.shipping.methods');
+            Route::put('shipping-methods/{id}','SettingsController@updateShippingMethods')->name('update.shipping.methods');
+    });
 });
 
-Route::group(['namespace' => 'Dashboard', 'middleware' => 'guest:admin'],function (){
-    Route::get('/login','LoginController@login') -> name('admin.login');
-    Route::post('/login','LoginController@postLogin') -> name('admin.post.login');
+        Route::group(['namespace' => 'Dashboard', 'middleware' => 'guest:admin', 'prefix' => 'admin'],function (){
+            Route::get('/login','LoginController@login') -> name('admin.login');
+            Route::post('/login','LoginController@postLogin') -> name('admin.post.login');
+});
+
 });
