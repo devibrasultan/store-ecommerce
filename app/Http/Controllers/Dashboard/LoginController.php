8b837c3778b9bcd7f47d5522833b75691cfca886
devibrasultan <devibrasultan@gmail.com>
2020-08-30 14:27:22 +0300

    =settings and authentication redirect

--- app/Http/Controllers/Dashboard/LoginController.php
+++ app/Http/Controllers/Dashboard/LoginController.php
@@ -22,4 +22,16 @@ class LoginController extends Controller
         // notify()->error('خطا في البيانات  برجاء المجاولة مجدا ');
         return redirect()->back()->with(['error' => 'هناك خطا بالبيانات']);
     }
+
+    public function logout(){
+        $guard = $this -> getGaurd();
+        $guard -> logout();
+
+        return redirect() -> route('admin.login');
+    }
+
+    private function getGaurd()
+    {
+        return auth('admin');
+    }
 }
